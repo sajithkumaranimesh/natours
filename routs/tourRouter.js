ec91e9cf69ef5882ad5f5506d58cccf816572bc2
Nimesh sajith kumara <sajithkumaranimesh@gmail.com>
2024-11-13 12:28:55 +0530

    connect mongoDB database and impliment post request and response

--- routs/tourRouter.js
+++ routs/tourRouter.js
@@ -3,13 +3,13 @@ const tourController = require("../controller/tourController");
 
 const router = express.Router();
 
-router.param('id', tourController.checkId)
+// router.param('id', tourController.checkId)
 
 
 router
   .route("/")
   .get(tourController.getAllTours)
-  .post(tourController.checkBody, tourController.createTours);
+  .post(tourController.createTours);
 router
   .route("/:id")
   .get(tourController.getTours)
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-const dotenv = require('dotenv');
-dotenv.config({path: './config.env'});
-const app = require('./app');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+const app = require("./app");
 const port = process.env.PORT || 3000;
+
+mongoose
+  .connect(process.env.DATABASE_LOCAL)
+  .then(() => console.log("Database Connected!!"));
 
 
 
