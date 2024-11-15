f2550ae2ef3ffc7eb96cfdb88ed152341a3befc2
Nimesh sajith kumara <sajithkumaranimesh@gmail.com>
2024-11-15 07:07:17 +0530

    Virtual Properties.

--- models/tourModel.js
+++ models/tourModel.js
@@ -51,6 +51,13 @@ const tourSchema = new mongoose.Schema({
     default: new Date()
   },
   startDates: [Date]
+},{
+  toJSON: {virtuals: true},
+  toObject: {virtuals: true}
+});
+
+tourSchema.virtual('durationWeeks').get(function() {
+  return this.duration / 7;
 });
 
 const Tour = mongoose.model("Tour", tourSchema);
