59e08369750c900fece8dd29565642f03762c455
Nimesh sajith kumara <sajithkumaranimesh@gmail.com>
2024-11-23 20:58:18 +0530

    impliment delete function in tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -88,6 +88,7 @@ exports.updateTour = async (req, res) => {
   try {
     const updatedTour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
+      runValidators: true,
     });
 
     res.status(200).json({
@@ -104,9 +105,17 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: "success",
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "FAILD",
+      messag: "Invalid data send!",
+    });
+  }
 };
