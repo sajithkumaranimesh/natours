3cc12279bd941964908f2f649cbdbe93a8fd9ca4
Nimesh sajith kumara <sajithkumaranimesh@gmail.com>
2024-11-23 12:29:19 +0530

    seperate tour model

--- server.js
+++ server.js
@@ -10,36 +10,6 @@ mongoose.connect(DB).then((con) => {
   console.log("DB connection successfuly!");
 });
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "A tour must have a name"],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, "A tour must have a price"],
-  },
-});
-
-const Tour = mongoose.model("Tour", tourSchema);
-
-const testTour = new Tour({
-  name: "test tour 1",
-  rating: 5.0,
-  price: 100,
-});
-
-testTour.save().then((doc) => {
-  console.log(doc);
-}).catch(err => {
-  console.log("Err : ", err);
-})
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
