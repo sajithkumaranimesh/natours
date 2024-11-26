2e895500f2c8878cf842ae092ebfe64a2ac573d3
Nimesh sajith kumara <sajithkumaranimesh@gmail.com>
2024-11-26 10:52:55 +0530

    create an alias in get top cheapest 5 tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -22,6 +22,14 @@ const Tour = require("../models/tourModel.js");
 //   next();
 // };
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = "-ratingsAverage,price";
+  req.query.fields = "name,price,ratingsAverage,summary,difficulty";
+  next();
+}
+
+
 exports.getAllTours = async (req, res) => {
   console.log(req.requestTime);
   console.log(req.query);
