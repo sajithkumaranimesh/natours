4a323337ad4cb9f63e37eae1d2fddc79d2f3f639
Nimesh sajith kumara <sajithkumaranimesh@gmail.com>
2024-11-27 10:30:49 +0530

    create global error handle class

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError')
+const globalErrorHandler = require('./controllers/errorController');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -30,10 +33,9 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'faild',
-    message: `Can't find ${req.originalUrl} on this section!`
-  })
+  next(new AppError(`Can't find ${req.originalUrl} on this section!`, 404));
 })
+
+app.use(globalErrorHandler);
 
 module.exports = app;
