905e229cec9c6f8b04807a3f160d6538f17b0840
Nimesh sajith kumara <sajithkumaranimesh@gmail.com>
2024-12-13 12:21:24 +0530

    data sanitization

--- app.js
+++ app.js
@@ -1,39 +1,46 @@
-const express = require('express');
-const morgan = require('morgan');
-const rateLimit = require('express-rate-limit');
-const helmet = require('helmet');
+const express = require("express");
+const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
-const AppError = require('./utils/appError')
-const globalErrorHandler = require('./controllers/errorController');
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 
-const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes');
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
 // 1) GLOBAL MIDDLEWARES
 
 // Set security HTTP headers
-app.use(helmet())
+app.use(helmet());
 
-
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 // Limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
-  message: 'Too many requests from this IP, please try again in an hour!'
-})
-app.use('/api', limiter);
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
 
 // Body parser, reading data from body into req.body
-app.use(express.json({limit: '10lb'}));
+app.use(express.json({ limit: "10kb" }));
 
+// Data sanitization against NoSql quey injection
+app.use(mongoSanitize());
+
+// data senitixation against XSS
+app.use(xss());
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
-
 
 // Test middleware
 app.use((req, res, next) => {
@@ -42,12 +49,12 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
 
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this section!`, 404));
-})
+});
 
 app.use(globalErrorHandler);
 
